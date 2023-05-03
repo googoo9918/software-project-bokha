aa92f3ec57039e50c4928443b697c37f8ace56e2
jungdonggyu <1013as@naver.com>
2023-05-03 16:35:32 +0900

    Delete: "템플릿 프로젝트에서 프로젝트에 필요 없는 내용 삭제"

--- server/src/main/java/com/app/global/config/web/WebConfig.java
+++ server/src/main/java/com/app/global/config/web/WebConfig.java
@@ -67,25 +67,5 @@ public class WebConfig implements WebMvcConfigurer {
         resolvers.add(memberInfoArgumentResolver);
     }
 
-    @Bean
-    public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
-        FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
-        filterRegistration.setFilter(new XssEscapeServletFilter());
-        filterRegistration.setOrder(1);
-        filterRegistration.addUrlPatterns("/*");
-        return filterRegistration;
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(jsonEscapeConverter());
-    }
-
-    @Bean
-    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
-        ObjectMapper copy = objectMapper.copy();
-        copy.getFactory().setCharacterEscapes(new HtmlCharacterEscapes());
-        return new MappingJackson2HttpMessageConverter(copy);
-    }
 
 }
