36418198d5c6c6011137f2b887c02d894aca0d56
imhwarang <imhwarang4@naver.com>
2023-05-07 23:27:58 +0900

    feat: make side bar in every page

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,26 +1,70 @@
 import React from "react";
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
+import PropTypes from "prop-types";
+import { makeStyles } from "@mui/styles";
+import { Box, Button, Typography } from "@mui/material";
 
-export default function Sidebar() {
+const useStyles = makeStyles({
+  root: {
+    position: "fixed",
+    left: 30,
+    marginTop: 80,
+  },
+  header: {
+    fontSize: 28,
+    marginBottom: 2,
+    marginLeft: 15,
+    color: "black",
+  },
+  divider: {
+    borderBottom: "1px solid black",
+    width: 250,
+    marginTop: 16,
+    marginBottom: 16,
+  },
+  navbarMenu: {
+    fontSize: 35,
+    marginTop: 20,
+    color: "black",
+  },
+});
+
+export default function Sidebar(props) {
+  const { items } = props;
+  const classes = useStyles();
+  const sidebarMenu = items.slice(1);
+
   return (
-    <Box
-      sx={{
-        mt: 12,
-        position: "fixed",
-        left: 30,
-      }}
-    >
-      <Box sx={{ fontSize: 24 }}>복지 검색 🔍</Box>
-      <Box sx={{ borderBottom: 1, width: 200, mt: 2, mb: 2 }}></Box>
-      <Box sx={{ fontSize: 24 }}>복지 검색</Box>
-      <Box
-        sx={{ borderBottom: 1, width: 200, mt: 2, mb: 2, color: "#bdbdbd" }}
-      ></Box>
-      <Box sx={{ fontSize: 24 }}>음성 검색</Box>
-      <Box
-        sx={{ borderBottom: 1, width: 200, mt: 2, mb: 2, color: "#bdbdbd" }}
-      ></Box>
+    <Box className={classes.root}>
+      <Box>
+        <Button>
+          <Box variant="h4" className={classes.navbarMenu}>
+            {items[0].title}
+          </Box>
+        </Button>
+        <Box className={classes.divider} />
+      </Box>
+      {sidebarMenu.map((item, index) => (
+        <Box key={index}>
+          <Button>
+            <Box variant="h6" className={classes.header}>
+              {item.title}
+            </Box>
+          </Button>
+          <Box className={classes.divider} />
+        </Box>
+      ))}
     </Box>
   );
 }
+
+Sidebar.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+Sidebar.defaultProps = {
+  items: [],
+};
