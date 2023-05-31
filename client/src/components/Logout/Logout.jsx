ffb5d6cdcad908439ef991f248df968b595463d7
imhwarang <imhwarang4@naver.com>
2023-05-31 20:57:18 +0900

    feat: Logout 컴포넌트 기능 구현 완료

--- client/src/components/Logout/Logout.jsx
+++ client/src/components/Logout/Logout.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import useAuth from "../../hook/useAuth";
+import axios from "axios";
+import { LogoutButton, LogoutContainer } from "./Logout.styled";
 
 export default function Logout() {
-  return <div>로그아웃 되었다</div>;
+  const { logout } = useAuth();
+  const accessToken = localStorage.getItem("accessToken");
+
+  const handleLogout = () => {
+    axios.post(
+      `${import.meta.env.VITE_APP_HOST}/api/logout`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    logout();
+  };
+
+  return (
+    <LogoutContainer>
+      <LogoutButton onClick={handleLogout}>로그아웃</LogoutButton>
+    </LogoutContainer>
+  );
 }
