c82afb4784f976590d8220af3f6eafd89135f050
imhwarang <imhwarang4@naver.com>
2023-06-01 01:25:20 +0900

    fix: recoil-persist 사용해서 새로고침시 상태저장 유지

--- client/src/recoil/user.js
+++ client/src/recoil/user.js
@@ -1,6 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const isLoggedInState = atom({
   key: "isLoggedInState",
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
