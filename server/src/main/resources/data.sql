02c35d9e2c7b8054703e810fcced64ceae15d08c
jungdonggyu <1013as@naver.com>
2023-06-03 15:01:08 +0900

    Fix: "배포 설정 파일 수정"

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,4 +1,2 @@
 INSERT INTO member(MEMBER_TYPE,EMAIL, PASSWORD, ROLE, MEMBER_NAME)
 VALUES ('DEFAULT','a1@admin.com', 'aaaa1111!', 'ADMIN', '관리자');
-
-INSERT INTO program(serv_nm,serv_id,member_id) values ('독거노인 목욕비 지원','WLF00002103',1);
