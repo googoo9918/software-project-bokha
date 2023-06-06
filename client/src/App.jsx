e86ce3e84d6c91d7480481725cd2b447b78f596b
imhwarang <imhwarang4@naver.com>
2023-06-06 16:59:26 +0900

    fix:페이지 이동시 최상단에서 시작하도록 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,16 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import GlobalStyle from "./components/GlobalStyle/GlobalStyle";
 import { RecoilRoot } from "recoil";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <RecoilRoot>
       <Navbar />
